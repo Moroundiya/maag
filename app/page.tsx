2f59f108a18a0136e9f8e05f0f1dc3e0b4881e13
Moroundiya <moroundiya@outlook.com>
2025-07-03 20:19:19 +0200

    Navbar design

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,25 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <nav className="bg-red-400 text-white ">
+      <nav className="bg-red-400 text-white flex justify-between items-center px-4 py-3">
         <div className="w-[100px] h-[50px] bg-amber-500 relative">
           <Link href="/about" className="">
             <Image src="/logo-dark.webp" priority fill alt="Logo" />
           </Link>
         </div>
+
+        <ul className="flex space-x-4 font-semibold text-lg">
+          <li>HOME</li>
+          <li>ENTERTAINMENT</li>
+          <li>TECH</li>
+          <li>POLITICS</li>
+          <li>HEALTH</li>
+          <li>LIFESTYLE</li>
+          <li>FOOD</li>
+        </ul>
+
+
+        
       </nav>
     </>
   );
