5091d2675a0d0fa387bf1879498b218f760f42e4
Moroundiya <moroundiya@outlook.com>
2025-07-06 20:19:19 +0200

    Hero Section design

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+
 export default function HeroSection() {
     return (
-        <div className="w-full h-[400px] bg-red-400">
-
+        <div className="w-full bg-red-400 px-12 grid grid-cols-2">
+            <div className="bg-blue-700 relative w-4/5">
+                {/* <Image src='/demo-image-1.webp' alt="Hero_Section_Image" fill className="" /> */}
+                <img src="/demo-image-1.webp" alt="" className="" />
+                <h1>News</h1>
+            </div>
         </div>
     );
 }
