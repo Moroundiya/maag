8aff9defb4bc971056b5d7582459d40358572ec9
Moroundiya <moroundiya@outlook.com>
2025-07-07 20:19:19 +0200

    Hero Section design

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,12 +1,12 @@
 
 export default function HeroSection() {
     return (
-        <div className="w-full bg-red-400 px-12 grid grid-cols-2">
-            <div className="relative text-white">
-                <img src="/demo-image-1.webp" alt="" className="" />
-                <h1 className="font-bold text-5xl pt-4">How Music Tours Create Unforgettable Moments</h1>
-                <p>In the Age of Information, news media faces both unprecedented opportunities and significant challenges.</p>
-                <div className="w-full border-b border-[#00000039] mt-10"></div>
+        <div className="w-full px-12 grid grid-cols-2 pt-10 mb-20">
+            <div className="relative">
+                <img src="/demo-image-1.webp" alt="" className="rounded-md w-full h-[400px]" />
+                <h1 className="font-bold text-5xl pt-4 pb-2">How Music Tours Create Unforgettable Moments</h1>
+                <p className="text-[#525252] leading-tight">In the Age of Information, news media faces both unprecedented opportunities and significant challenges.</p>
+                <div className="w-full border-b border-[#00000039] my-4"></div>
             </div>
         </div>
     );
