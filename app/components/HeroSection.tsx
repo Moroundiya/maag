21d0fbbdb9408863df24220c40f56e46cc60ef53
Moroundiya <moroundiya@outlook.com>
2025-07-10 20:19:19 +0200

    Hero Section design

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Eye } from 'lucide-react';
 import Medium from "../layouts/HeroMedium";
+import HeroSmall from "../layouts/HeroSmall";
 
 export default function HeroSection() {
     return (
@@ -32,21 +33,9 @@ export default function HeroSection() {
                     <Medium title="Exploring the Evolution of Jazz: From Classic to Contemporary" date="Jan 7, 2025" category="Events" author="Joanna Wellick" views={3} image="/demo-image-3.webp" />
                     <Medium title="The Best Rock Albums Released This Year: Must-Listen Tracks and Standout Records" date="Jan 7, 2025" category="Events" author="Joanna Wellick" views={3} image="/demo-image-2.webp" />
                 </div>
-                <div className="space-y-8 pl-3">
-                    <div className="flex space-x-3">
-                        <div className="w-full h-[100px] relative overflow-hidden rounded-md">
-                            <Image src="/demo-image-4.webp" alt="" fill className="rounded-md object-cover hover:scale-105 transition-all duration-500" />
-                        </div>
-                        <div className="space-y-2">
-                            <p className="text-md leading-snug">The Lure of Nostalgia: How Retro Music Comebacks Are Shaping Today’s Soundscape</p>
-                            <div className="flex items-center space-x-3 text-[12px]">
-                                <div className="bg-[#0000002b] px-2 py-0.5 rounded-sm">
-                                    Events
-                                </div>
-                                <p>Jan 7, 2025</p>
-                            </div>
-                        </div>
-                    </div>
+                <div className="space-y-4 pl-3">
+                    <HeroSmall title="The Rise of Indie Music: How Independent Artists Are Changing the Industry" date="Jan 7, 2025" category="Events" image="/demo-image-5.webp" />
+                    <HeroSmall title="The Lure of Nostalgia: How Retro Music Comebacks Are Shaping Today’s Soundscape" date="Jan 7, 2025" category="Events" image="/demo-image-4.webp" />
                 </div>
 
             </div>
--- app/layouts/HeroSmall.tsx
+++ app/layouts/HeroSmall.tsx
@@ -2,12 +2,12 @@ import Image from "next/image"
 
 export default function HeroSmall({ title, date, category, image }: { title?: string; date?: string; category?: string, image?: string }) {
     return (
-        <div className="flex space-x-3">
-            <div className="w-full h-[100px] relative overflow-hidden rounded-md">
+        <div className="flex space-x-3 border-b border-[#00000027] pb-4">
+            <div className="w-full h-[100px] xl:h-[80px] 2xl:h-[100px] relative overflow-hidden rounded-md">
                 <Image src={image ? image : ''} alt="" fill className="rounded-md object-cover hover:scale-105 transition-all duration-500" />
             </div>
             <div className="space-y-2">
-                <p className="text-md leading-snug">{title}</p>
+                <p className="text-md leading-snug hover:text-[#0000008d] transition-all duration-300 font-bold">{title}</p>
                 <div className="flex items-center space-x-3 text-[12px]">
                     <div className="bg-[#0000002b] px-2 py-0.5 rounded-sm">
                         {category}
