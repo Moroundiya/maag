bf8acef281c156091ffbc11ff8c548bbab22ac4d
Moroundiya <moroundiya@outlook.com>
2025-07-12 20:19:19 +0200

    Entertainment section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import { ArrowRight } from "lucide-react";
 import HeroSection from "./components/HeroSection";
 import Separator from "./layouts/Seperator";
-import HeroMedium from "./layouts/HeroMedium";
-import HeroSmall from "./layouts/HeroSmall";
 import Entertainment from "./components/Entertainment";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,6 +10,13 @@ export default function Home() {
       <HeroSection />
       <Separator />
       <Entertainment />
+      <Separator />
+      <div className="px-3 xl:px-8 2xl:px-12 font-semibold mb-10">
+        <Link href="/tech" className="flex items-center">
+          <span>Tech</span>
+          <ArrowRight size={20} />
+        </Link>
+      </div>
     </>
   );
 }
