0d634064aa1e3c0a2dcc1f50c2717ae4e5c901b4
Moroundiya <moroundiya@outlook.com>
2025-07-14 20:19:19 +0200

    Tech section

--- app/components/Tech.tsx
+++ app/components/Tech.tsx
@@ -1,4 +1,5 @@
 import { ArrowRight, Eye } from "lucide-react"
+import Image from "next/image"
 import Link from "next/link"
 
 export default function Tech() {
@@ -9,28 +10,28 @@ export default function Tech() {
                 <ArrowRight size={20} />
             </Link>
 
-            <div className="w-full h-[500px] mt-5 flex justify-between relative group overflow-hidden">
-                <div className="absolute top-0 left-0 w-full h-full bg-[url(/section-bg.webp)] bg-center bg-no-repeat bg-cover group-hover:scale-105 transition-all duration-700 ease-in-out">
-
-                </div>
-                <div className="p-10 w-2/4">
-                    <h1 className="font-bold text-3xl leading-tight">Crafting Luxurious Bathrooms Without Sacrificing Practicality</h1>
-                    <div className="flex items-center mt-4">
-                        <div className="flex items-center space-x-3 text-[12px]">
-                            <div className="bg-[#0000002b] px-2 py-0.5 rounded-sm">
-                                Tech
-                            </div>
-                            <p>1 August, 2025.</p>
-                            <p>by Moroundiya</p>
-                            <div className="flex items-center justify-center space-x-0.5 text-[11.5px]">
-                                <Eye className="" size={12} />
-                                <p>3</p>
+            <div className="w-full h-[600px] mt-5 relative group overflow-hidden">
+                <Image src="/section-bg.webp" alt="Image" fill className="absolute top-0 left-0 group-hover:scale-105 transition-all duration-700 ease-in-out" />
+                <div className="absolute w-full h-full top-0 left-0 bg-[#00000048] flex justify-between">
+                    <div className="p-10 w-2/4">
+                        <h1 className="font-bold text-3xl leading-tight">Crafting Luxurious Bathrooms Without Sacrificing Practicality</h1>
+                        <div className="flex items-center mt-4">
+                            <div className="flex items-center space-x-3 text-[12px]">
+                                <div className="bg-[#0000002b] px-2 py-0.5 rounded-sm">
+                                    Tech
+                                </div>
+                                <p>1 August, 2025.</p>
+                                <p>by Moroundiya</p>
+                                <div className="flex items-center justify-center space-x-0.5 text-[11.5px]">
+                                    <Eye className="" size={12} />
+                                    <p>3</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                    <p>Popular Posts</p>
 
-                <p>Popular Posts</p>
+                </div>
             </div>
         </div>
     )
