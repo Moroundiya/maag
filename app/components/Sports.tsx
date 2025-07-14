f703c919bf1f99288b34657be259be096256cca1
Moroundiya <moroundiya@outlook.com>
2025-07-14 20:19:19 +0200

    Sport section

--- app/components/Sports.tsx
+++ app/components/Sports.tsx
@@ -1,5 +1,32 @@
+import { ArrowRight } from "lucide-react"
+import Link from "next/link"
+import HeroMedium from "../layouts/HeroMedium"
+import HeroSmall from "../layouts/HeroSmall"
+
 export default function Sports() {
     return (
-        
+        <div className="px-3 xl:px-8 2xl:px-12 font-semibold mb-10">
+            <Link href="/entertainment" className="flex items-center">
+                <span>Entertainment</span>
+                <ArrowRight size={20} />
+            </Link>
+            <div className="grid grid-cols-3 mt-5">
+                <div className="space-y-8 border-r pr-4 border-[#00000027]">
+                    <HeroMedium title="Exploring the Evolution of Jazz: From Classic to Contemporary" date="Jan 7, 2025" category="Events" author="Joanna Wellick" views={3} image="/demo-image-3.webp" />
+                    <HeroSmall title="The Rise of Indie Music: How Independent Artists Are Changing the Industry" date="Jan 7, 2025" category="Events" image="/demo-image-5.webp" />
+                    <HeroSmall title="Behind the Scenes: The Making of a Blockbuster Hit" date="Jan 7, 2025" category="Events" image="/demo-image-6.webp" />
+                </div>
+                <div className="space-y-8 px-4">
+                    <HeroMedium title="Exploring the Evolution of Jazz: From Classic to Contemporary" date="Jan 7, 2025" category="Events" author="Joanna Wellick" views={3} image="/demo-image-3.webp" />
+                    <HeroSmall title="The Rise of Indie Music: How Independent Artists Are Changing the Industry" date="Jan 7, 2025" category="Events" image="/demo-image-5.webp" />
+                    <HeroSmall title="Behind the Scenes: The Making of a Blockbuster Hit" date="Jan 7, 2025" category="Events" image="/demo-image-6.webp" />
+                </div>
+                <div className="space-y-8 ps-4 border-l border-[#00000027]">
+                    <HeroMedium title="Exploring the Evolution of Jazz: From Classic to Contemporary" date="Jan 7, 2025" category="Events" author="Joanna Wellick" views={3} image="/demo-image-3.webp" />
+                    <HeroSmall title="The Rise of Indie Music: How Independent Artists Are Changing the Industry" date="Jan 7, 2025" category="Events" image="/demo-image-5.webp" />
+                    <HeroSmall title="Behind the Scenes: The Making of a Blockbuster Hit" date="Jan 7, 2025" category="Events" image="/demo-image-6.webp" />
+                </div>
+            </div>
+        </div>
     )
 }
