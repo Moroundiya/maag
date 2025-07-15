9d89a96785011f9c524f469b9489acd02ecbf21f
Moroundiya <moroundiya@outlook.com>
2025-07-15 20:19:19 +0200

    Tech section

--- app/components/Tech.tsx
+++ app/components/Tech.tsx
@@ -1,6 +1,7 @@
 import { ArrowRight, Eye } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+import HeroSmall from "../layouts/HeroSmall"
 
 export default function Tech() {
     return (
@@ -10,7 +11,7 @@ export default function Tech() {
                 <ArrowRight size={20} />
             </Link>
 
-            <div className="w-full h-[600px] mt-5 relative group overflow-hidden">
+            <div className="w-full h-[500px] mt-5 relative group overflow-hidden">
                 <Image src="/section-bg.webp" alt="Image" fill className="absolute top-0 left-0 group-hover:scale-105 transition-all duration-700 ease-in-out" />
                 <div className="absolute w-full h-full top-0 left-0 bg-[#00000048] flex justify-between">
                     <div className="p-10 w-2/4 text-white">
@@ -20,7 +21,7 @@ export default function Tech() {
                                 <div className="bg-[#0000002b] px-2 py-0.5 rounded-sm">
                                     Tech
                                 </div>
-                                <p className="text-gray-300 font-light">1 August, 2025.</p>
+                                <p className="text-gray-300 font-light">1 August, 2025</p>
                                 <p className="text-gray-300 font-light">by Moroundiya</p>
                                 <div className="flex items-center justify-center space-x-0.5 text-[11.5px]">
                                     <Eye className="" size={12} />
@@ -29,7 +30,51 @@ export default function Tech() {
                             </div>
                         </div>
                     </div>
-                    <p>Popular Posts</p>
+                    <div className="border-s border-[#dedede8d] w-2/7 flex flex-col justify-between text-white">
+                        <p className="py-10 border-b border-[#dedede8d] text-xl font-semibold px-6">Popular Posts</p>
+                        <div className="flex space-x-3 border-b border-[#dedede8d] pb-4 px-6">
+                            <div className="w-full lg:w-1/2 h-[100px] xl:h-[80px] 2xl:h-[100px] relative overflow-hidden rounded-md">
+                                <Image src='/demo-image-1.webp' alt="" fill className="rounded-md object-cover hover:scale-105 transition-all duration-500" />
+                            </div>
+                            <div className="w-full lg:w-2/3 space-y-2">
+                                <p className="text-md leading-snug hover:text-[#dedede8d] transition-all duration-300 font-bold">Crafting Luxurious Bathrooms Without Sacrificing Practicality</p>
+                                <div className="flex items-center space-x-3 text-[11.5px]">
+                                    <div className="bg-[#0000002b] px-1.5 py-0.5 rounded-sm">
+                                        Tech
+                                    </div>
+                                    <p className="text-gray-300 font-light">1 August, 2025</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex space-x-3 border-b border-[#dedede8d] pb-4 px-6">
+                            <div className="w-full lg:w-1/2 h-[100px] xl:h-[80px] 2xl:h-[100px] relative overflow-hidden rounded-md">
+                                <Image src='/demo-image-1.webp' alt="" fill className="rounded-md object-cover hover:scale-105 transition-all duration-500" />
+                            </div>
+                            <div className="w-full lg:w-2/3 space-y-2">
+                                <p className="text-md leading-snug hover:text-[#dedede8d] transition-all duration-300 font-bold">Crafting Luxurious Bathrooms Without Sacrificing Practicality</p>
+                                <div className="flex items-center space-x-3 text-[11.5px]">
+                                    <div className="bg-[#0000002b] px-1.5 py-0.5 rounded-sm">
+                                        Tech
+                                    </div>
+                                    <p className="text-gray-300 font-light">1 August, 2025</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex space-x-3 border-b border-[#dedede8d] pb-4 px-6">
+                            <div className="w-full lg:w-1/2 h-[100px] xl:h-[80px] 2xl:h-[100px] relative overflow-hidden rounded-md">
+                                <Image src='/demo-image-1.webp' alt="" fill className="rounded-md object-cover hover:scale-105 transition-all duration-500" />
+                            </div>
+                            <div className="w-full lg:w-2/3 space-y-2">
+                                <p className="text-md leading-snug hover:text-[#dedede8d] transition-all duration-300 font-bold">Crafting Luxurious Bathrooms Without Sacrificing Practicality</p>
+                                <div className="flex items-center space-x-3 text-[11.5px]">
+                                    <div className="bg-[#0000002b] px-1.5 py-0.5 rounded-sm">
+                                        Tech
+                                    </div>
+                                    <p className="text-gray-300 font-light">1 August, 2025</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
