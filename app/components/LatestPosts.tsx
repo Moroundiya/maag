59bbb3f81389bd55a32003dfd34234ec48f55231
Moroundiya <moroundiya@outlook.com>
2025-07-18 20:19:19 +0200

    Latest posts section

--- app/components/LatestPosts.tsx
+++ app/components/LatestPosts.tsx
@@ -11,7 +11,10 @@ export default function LatestPosts() {
                 <ArrowRight size={20} />
             </Link>
             <div className="w-full mt-5 grid grid-cols-4">
-
+                <HeroMedium title="Exploring the Evolution of Jazz: From Classic to Contemporary" date="Jan 7, 2025" category="Events" author="Joanna Wellick" views={3} image="/demo-image-3.webp" />
+                <HeroMedium title="Exploring the Evolution of Jazz: From Classic to Contemporary" date="Jan 7, 2025" category="Events" author="Joanna Wellick" views={3} image="/demo-image-3.webp" />
+                <HeroMedium title="Exploring the Evolution of Jazz: From Classic to Contemporary" date="Jan 7, 2025" category="Events" author="Joanna Wellick" views={3} image="/demo-image-3.webp" />
+                <HeroMedium title="Exploring the Evolution of Jazz: From Classic to Contemporary" date="Jan 7, 2025" category="Events" author="Joanna Wellick" views={3} image="/demo-image-3.webp" />
             </div>
         </div>
 
