ce2264a54abc7ae6421153c825c0652b20411a48
Moroundiya <moroundiya@outlook.com>
2025-07-18 20:19:19 +0200

    Latest posts section

--- app/components/LatestPosts.tsx
+++ app/components/LatestPosts.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import { Eye, ArrowRight } from 'lucide-react';
 import HeroMedium from "../layouts/HeroMedium";
 import Link from "next/link";
 
@@ -7,10 +5,9 @@ export default function LatestPosts() {
     return (
         <div className="px-3 xl:px-8 2xl:px-12 font-semibold mb-10">
             <Link href="/health" className="flex items-center">
-                <span>Health</span>
-                <ArrowRight size={20} />
+                <span>Latest Posts</span>
             </Link>
-            <div className="w-full mt-5 grid grid-cols-4">
+            <div className="w-full mt-5 grid grid-cols-4 gap-8">
                 <HeroMedium title="Exploring the Evolution of Jazz: From Classic to Contemporary" date="Jan 7, 2025" category="Events" author="Joanna Wellick" views={3} image="/demo-image-3.webp" />
                 <HeroMedium title="Exploring the Evolution of Jazz: From Classic to Contemporary" date="Jan 7, 2025" category="Events" author="Joanna Wellick" views={3} image="/demo-image-3.webp" />
                 <HeroMedium title="Exploring the Evolution of Jazz: From Classic to Contemporary" date="Jan 7, 2025" category="Events" author="Joanna Wellick" views={3} image="/demo-image-3.webp" />
