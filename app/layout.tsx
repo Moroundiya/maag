95c19303889186d345cca46d05d5b98c89e1fd74
Moroundiya <moroundiya@outlook.com>
2025-07-20 20:19:19 +0200

    Hero section mobile responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,14 +25,14 @@ export default function RootLayout({
   return (
     <html lang="en" className={familjen.className}>
       <body>
-        <nav className="flex justify-between items-center xl:px-8 2xl:px-12 py-6 border-b-2 shadow-xl fixed z-20 top-0 left-0 w-full border-black bg-[#f7f7e8] transition-all duration-700 ease-in-out">
-          <div className="w-[90px] h-[40px] relative">
+        <nav className="flex justify-between items-center px-3 py-3 xl:px-8 2xl:px-12 lg:py-6 border-b-2 shadow-xl fixed z-20 top-0 left-0 w-full border-black bg-[#f7f7e8] transition-all duration-700 ease-in-out">
+          <div className="w-[70px] h-[30] sm:w-[90px] sm:h-[40px] relative">
             <Link href="/" className="">
-              <Image src="/logo-dark.webp" priority fill alt="Logo" />
+              <Image src="/logo-dark.webp" priority fill alt="Logo" className="" />
             </Link>
           </div>
-          {/* <Menu size={26} /> */}
-          <ul className="flex space-x-6 font-semiboldd text-xl text-black">
+          <Menu size={26} className="lg:hidden" />
+          <ul className="hidden lg:flex space-x-6 text-xl text-black">
             {
               links.map((link) => {
                 return (
@@ -42,8 +42,7 @@ export default function RootLayout({
               })
             }
           </ul>
-
-          <Link href="/login" className="flex items-center space-x-2 justify-center bg-[#000] text-white px-3 py-2 rounded-md text-sm cursor-pointer hover:bg-[#2e2c2c] transition-all duration-500 ease-in-out">
+          <Link href="/login" className="hidden lg:flex items-center space-x-2 justify-center bg-[#000] text-white px-3 py-2 rounded-md text-sm cursor-pointer hover:bg-[#2e2c2c] transition-all duration-500 ease-in-out">
             <SquarePen className="cursor-pointer" size={20} />
             <p className="">Post Content</p>
             {/* <div className="border-r border-[#12121272] h-3"></div>
