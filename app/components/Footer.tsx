5550e590dc7a5d7924d804e474d4a69a384576e6
Moroundiya <moroundiya@outlook.com>
2025-07-20 20:19:19 +0200

    Footer section

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     const date = new Date().getFullYear()
     return (
         <div className="w-full bg-[#888868] flex justify-between items-center px-3 xl:px-12 text-white p-2 font-semibold">
-            <p className="text-center w-full">Copyright - MAGG {date}</p>
+            <p className="text-center w-full">© Copyright - MAGG {date}</p>
         </div>
     )
 }
